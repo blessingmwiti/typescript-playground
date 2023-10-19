abdb288c82e2394bc1ffa052be95e0fc1f737750
blessingmwiti <blessingmwiti>
2023-10-19 23:05:20 +0300

    finalasima

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,60 @@
 "use strict";
-let age = 20;
-if (age < 50)
-    age += 10;
+let greeting = 'Hello World';
+let sales = 123456789;
+let course = 'TypeScript';
+let isPublished = true;
+let arr = [3, 'TypeScript', false, 67];
+const small = 1;
+const medium = 2;
+const large = 3;
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+;
+let mySize = Size.Medium;
+console.log(mySize);
+function calcTax(income, taxYear = 2022) {
+    if (taxYear < 2022)
+        return income * 1.2;
+    return income * 1.3;
+}
+calcTax(10000);
+let employee = { id: 1,
+    name: '',
+    retire: (date) => {
+        console.log(date);
+    } };
+employee.name = 'Blessing';
+function kgToLbs(weight) {
+    if (typeof weight === 'number') {
+        return weight * 2.2;
+    }
+    else {
+        return parseInt(weight) * 2.2;
+    }
+}
+kgToLbs(10);
+kgToLbs('10kg');
+let textBox = {
+    drag: () => { },
+    resize: () => { }
+};
+let quantity = 100;
+function greet(name) {
+    if (name)
+        console.log(name.toUpperCase());
+    else
+        console.log('Hola');
+}
+greet('Hello muchacho');
+greet(null);
+greet(undefined);
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(1);
+console.log(customer === null || customer === void 0 ? void 0 : customer.birthday);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,98 @@ let greeting: string = 'Hello World';
 let sales = 123_456_789;
 let course = 'TypeScript';
 let isPublished = true;
-let level;
+
+// types
+let arr: [number, string, boolean, number] = [3, 'TypeScript', false, 67];
+
+// enum
+const small = 1;
+const medium = 2;
+const large = 3;
+
+enum Size {Small = 1, Medium, Large}; // in enum, the first word starts with a bloody capital
+let mySize: Size = Size.Medium;
+console.log(mySize);
+
+// function
+function calcTax (income: number, taxYear = 2022): number{
+    if (taxYear < 2022)
+    return income * 1.2;
+    return income * 1.3;
+}
+calcTax(10_000);
+
+// objects and advanced types
+type Employee ={
+    readonly id: number, // readonly, id cannot be changed
+    name: string
+    retire: (date: Date) => void
+
+}
+let employee: Employee = {id: 1,
+     name: '',
+    retire: (date: Date) => {
+        console.log(date);
+    }};
+
+employee.name = 'Blessing';
+
+// union types
+function kgToLbs(weight: number | string): number {
+    // narrowing
+    if (typeof weight === 'number') {
+        return weight * 2.2;
+    }else{
+        return parseInt(weight) * 2.2; // parseInt converting string to int
+    }
+}
+
+kgToLbs(10);
+kgToLbs('10kg');
+
+// intersection types
+type Draggable = {
+    drag: () => void;
+};
+
+type Resizable = {
+    resize: () => void;
+}
+
+type UIWidget = Draggable & Resizable;
+
+let textBox: UIWidget = {
+    drag: () => {},
+    resize: () => {}
+}
+
+// literal types
+type Quantity = 50 | 100; // literal integers
+let quantity: Quantity = 100;
+
+type Metric = 'cm' | 'inch'; // literal strings
+
+// nullable types
+function greet(name: string | null | undefined){
+    if (name)
+    console.log(name.toUpperCase());
+else
+console.log('Hola');
+}
+
+greet('Hello muchacho');
+greet(null);
+greet(undefined);
+
+// optional chaining
+type Customer = {
+    birthday?: Date
+};
+
+function getCustomer(id: number): Customer | null | undefined{
+    return id === 0 ? null : {birthday: new Date()};
+}
+
+let customer = getCustomer(1);
+
+console.log(customer?.birthday?.getFullYear());
